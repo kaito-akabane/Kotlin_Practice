bc48b8e7fff591e2515d6f53d8e3ff07773ac42f
K-AKABANE <kesf0419@gmail.com>
2023-10-08 19:09:41 +0900

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n:Int = br.readLine().toInt()
+
+
+}
